48bb8f7d4faff1d2fb4718342adf4d5135a22597
Kaoz444 <damian_rocket@hotmail.com>
2025-01-21 19:29:50 -0600

    Update supabaseClient.js

--- backend/supabaseClient.js
+++ backend/supabaseClient.js
@@ -1,8 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseServiceKey = process.env.SUPABASE_SERVICE_KEY;
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_KEY; // Si es necesario
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
 
 // Admin client for backend operations
 export const supabaseAdmin = createClient(supabaseUrl, supabaseServiceKey);
